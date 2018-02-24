801e0f71de47012d9319015ca890bc0f657bc9aa
Adrián Tóth <adricko28@gmail.com>
2018-02-24 14:41:07 +0100

    yeaaaa

--- loop2.rb
+++ loop2.rb
@@ -1,23 +1,11 @@
 a           =         0
-while a < 15
-    print a, " "
-    if a == 10 then
+while   a < 15
+    print a," "
+    if a == 10   then
         print "made it to ten!!"      
     end
-    a = a + 1          
+    a = a   + 1          
 end
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 print "\n"
