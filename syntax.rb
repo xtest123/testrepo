27a8b5dd75628dca196342a9243e423ad2011940
Adrián Tóth <adricko28@gmail.com>
2018-04-24 10:32:03 +0200

    asd

--- syntax.rb
+++ syntax.rb
@@ -1,8 +1,10 @@
+   asd if a == 10 then
+    a = a + 1
+asda
 CONFLIIIIIIIIIIIIICT a < 15
 	(
-print a, " "
-    if a == 10 then
-        prin"made it to txen!! CONFLIIIIIIIIIIIIICT"
-
-    a = a + 1
 end
+print a, " "
+     asd   prin"made it to tx
+
+     en!! CONFLIIIIIIIIIIIIICT"
